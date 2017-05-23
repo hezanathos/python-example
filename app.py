6d2f15159ee87b849c2241363b373494bf6b9e98
hezanathos <hezanathos@gmail.com>
2017-05-23 10:24:21 +0200

    sec

---
+++ app.py
@@ -0,0 +1,8 @@
+import bottle
+from bottle import route, run, template
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+run(host='localhost', port=8080)
